a5ac9cf488a9ead1dd460de270ab7a47205cefc2
Selvakumar Jeremiah <Selvakumar>
2018-07-03 01:25:10 +0800

    python if statement

---
+++ ifcond.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+def main():
+	x,y =12,8
+	
+	if(x < y):
+		st= "x is less than y"
+        else:
+                st= "x is greater than y"
+	print st
+	
+if __name__ == "__main__":
+	main()
