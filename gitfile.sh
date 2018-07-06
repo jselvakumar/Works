8f229d85a31a7fcf1d1a098634edc992cfe9b9e1
Selvakumar Jeremiah <Selvakumar>
2018-07-07 03:02:11 +0800

    newfiles

--- gitfile.sh
+++ gitfile.sh
@@ -5,11 +5,12 @@ do
 for BR in $(git branch -a | sed -e's/*//')
 do
 count=$(git ls-tree -r $BR --name-only | grep ST | wc -l)
+#echo $count
 if [ $count == 0 ]
 then
-echo "$ST is not present $BR"
-else
 echo "$ST is present in $BR"
+else
+echo "$ST is not present in $BR"
 fi
 done
 done
--- writefile.py
+++ writefile.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-staging = open("newfile.txt","w")
+staging = open("file.txt","w")
 input = ["typical input given to new file \n", "next line 1 \n", "next line 2 \n"]
 staging.writelines(input)
 staging.close()
