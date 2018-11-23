23d83d74c95cd334ceb0b20374e8a4414c138eb3
jselvakumar <selvajere@gmail.com>
2018-11-23 15:58:45 +0530

    Update jira_issue.py

--- jira_issue.py
+++ jira_issue.py
@@ -1,20 +1,18 @@
 import sys
-sys.path.append ("\\\\kunx0014\\migration\\python\\lib\\3.4\\site-packages")
+sys.path.append ("\\\\older\\migration\\python\\lib\\3.4\\site-packages")
 from jira import JIRA
-jira = JIRA(basic_auth=('e5350756', 'Time2!@#123'), server='http://gfr4repsatlap01.kordoba.de:8090')
+jira = JIRA(basic_auth=('selvakuj', 'xxxxx'), server='http://gxxxx.xx:8090')
 
 for i in range(1):        
     summary = "XREF ticket -" + str(i)
     issue_dict = {
-    'project': {'key': 'KDBJAVA'},
+    'project': {'key': 'JIRA_JAVA'},
     'summary': summary,
-    'parent' : {'key' : 'KDBJAVA-29'},
+    'parent' : {'key' : 'JIRA_JAVA-29'},
     'issuetype': {'name': 'Sub-task'},
     'priority' : {'id' : '3'},
-    #'customfield_10178' : {'value' : 'BANKCONTROL'},
-    #'customfield_10176' : {'value' : 'ING KGS'},
-    'customfield_10198' : {'value' : 'ecore000'}
+    'customfield_10198' : {'value' : 'mandatory'}
     }
     new_issue = jira.create_issue(fields=issue_dict)
-    jira.assign_issue(new_issue, 'e5350756')
+    jira.assign_issue(new_issue, 'selvakuj')
     print(new_issue.key)
