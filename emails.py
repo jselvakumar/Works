5c92e5c15857cdfbc706dbe8315fdc7903f3ce50
Selvakumar Jeremiah <Selvakumar>
2019-04-08 02:19:31 +0800

    .

--- emails.py
+++ emails.py
@@ -5,6 +5,9 @@ emil = open("/Users/selvakumarjeremiah/email.txt", "r")
 #print emil.readlines()
 for emils in emil:
     regx = re.findall(r'[\w\.-]+@[\w\.-]+', emils)
-    print (regx)
+    for empty in regx:
+        if empty!=[]:
+           empty.strip()   
+    print (empty)
     
 
