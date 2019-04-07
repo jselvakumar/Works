794485977474ac3b21885930fd31cd3578ede604
Selvakumar Jeremiah <Selvakumar>
2019-04-08 01:18:54 +0800

    latest

---
+++ date.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import sys
+#import os.command
+import os
+cmd = "date"
+
+date = os.system(cmd)
+
+#print ('date') 
---
+++ reversename.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+fname = input("Input your First Name : ")
+lname = input("input your last Name : ")
+print ("lname,fname")
+
