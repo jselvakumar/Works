e62a2f20ab35011df3e25c0f25594d281c708907
jselvakumar <selvajere@gmail.com>
2019-04-23 18:26:21 +0530

    Update getapi.py

--- getapi.py
+++ getapi.py
@@ -11,7 +11,12 @@ reqget = requests.get(url)
 reqgetjson = reqget.json()
 
 clone = reqgetjson["owner"]
-
+ineed=['clone_url','created_at','updated_at','pushed_at']
+for detail in ineed:
+    if detail in reqgetjson:
+#        print ("%s : %s"%(detail,reqgetjson[detail]))
+         print (detail,":",reqgetjson[detail])
+#pprint.pprint (clone)
 pprint.pprint (clone)
 
 #pprint.pprint (reqgetjson)
